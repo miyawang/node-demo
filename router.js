8a4c702a405c0360bacc90c6df0887f883c7c9ad
miyawang <miyawang1019@gmail.com>
2018-07-12 19:51:28 +0800

    debug

--- router.js
+++ router.js
@@ -12,17 +12,3 @@ function route(handle, pathname, response,request) {
 
 exports.route = route;
 
-
-// function route(handle,pathname,response) {
-//   console.log("路由请求 " + pathname);
-//   if (typeof handle[pathname] === 'function') {
-//     return handle[pathname]();
-//   } else {
-//     console.log("没有找到这个请求 " + pathname);
-//     response.writeHead(404, {"Content-Type": "text/plain"});
-//     response.write("404 Not found");
-//     response.end();
-//   }
-// }
-
-// exports.route = route;
--- server.js
+++ server.js
@@ -8,32 +8,9 @@ function start(route, handle) {
 
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
-
-    // request.setEncoding("utf8");
-
-    // request.addListener("data", function(postDataChunk) {
-    //   postData += postDataChunk;
-    //   console.log("Received POST data chunk '"+
-    //   postDataChunk + "'.");
-    // });
-
-    // request.addListener("end", function() {
-    //   route(handle, pathname, response, postData);
-    // });
     route(handle, pathname, response,request);
     
   }
-  // function route(handle, pathname, response, postData) {
-  //   console.log("About to route a request for " + pathname);
-  //   if (typeof handle[pathname] === 'function') {
-  //     handle[pathname](response, postData);
-  //   } else {
-  //     console.log("No request handler found for " + pathname);
-  //     response.writeHead(404, {"Content-Type": "text/plain"});
-  //     response.write("404 Not found");
-  //     response.end();
-  //   }
-  // }
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started.");
